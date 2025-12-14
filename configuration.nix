4300d4d80429e0d86eb3fe81ca3e6d280b4c17ed
strize07 <strizeop@gmail.com>
2025-12-14 20:56:08 +0530

    t

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
 
   #to run localsend
   nixpkgs.config.permittedInsecurePackages = [
-    "broadcom-sta-6.30.223.271-59-6.12.58"
+    "broadcom-sta-6.30.223.271-59-6.12.61"
   ];
 
   # Set your time zone.
